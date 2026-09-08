a9e5d50eb05dc3ccd8cfbd3e75590894bbb45554
KI-Anik <iamkhairul101@gmail.com>
2026-09-08 20:50:30 +0600

    feat: add SVG icons for various platforms and create initial app structure
    - Added SVG icons for Bluesky, Discord, Documentation, GitHub, Social, and X.
    - Created App component with a TestComponent for demonstration.
    - Implemented CSS styles for the app layout and components.
    - Added global CSS variables for theming and responsive design.
    - Set up Vite configuration for React application.
    - Included hero image and React logo SVG assets.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import TestCompnent from "./TestCompnent"
+
+function App() {
+
+  return (
+    <>
+   <h1>Hello World</h1>
+   <TestCompnent></TestCompnent>
+    </>
+  )
+}
+
+export default App
---
+++ src/TestCompnent.jsx
@@ -0,0 +1,12 @@
+
+const country = 'Bangladesh'
+const TestCompnent = () => {
+    return (
+        <div>
+            Testing....
+            <h1>I live in {country}</h1>
+        </div>
+    );
+};
+
+export default TestCompnent;
