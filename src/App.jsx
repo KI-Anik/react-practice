af0936967d7143ff08edfb8f93d1488014303cdd
KI-Anik <iamkhairul101@gmail.com>
2026-09-08 23:42:29 +0600

    feat: implement SecondCard component and update App to display filtered members

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
 import Card from "./components/Card"
+import SecondCard from "./components/SecondCard"
 import TestCompnent from "./TestCompnent"
+
+
+const members = [
+  { name: "Rahim", age: 23 },
+  { name: "Rahim", age: 52 },
+  { name: "Rahim", age: 32 },
+  { name: "Rahim", age: 10 },
+]
 
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      {/* <h1>Hello World</h1>
       <TestCompnent></TestCompnent>
       <Card firstName="Amir"
         lastName="khan"
         age="40">
           Welcome
-      </Card>
+      </Card> */}
+
+      <div className="card">
+        {
+          members
+            .filter((member) => member.age > 18)
+            .sort((a, b) => a.age - b.age)
+            .map((member) => <SecondCard  name={member.name} age={member.age}></SecondCard>)
+        }
+      </div>
     </>
   )
 }
---
+++ src/components/SecondCard.jsx
@@ -0,0 +1,13 @@
+
+
+const SecondCard = ({name, age}) => {
+
+    return (
+        <div className="card">
+            <p>Name : {name}</p>
+            <p>Age : {age}</p>
+        </div>
+    );
+};
+
+export default SecondCard;
